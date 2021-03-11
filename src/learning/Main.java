38ace2eb9d3a50b8994806cd2592408d51b84aaf
ACER <hendry45xtc@gmail.com>
2021-03-11 15:28:53 +0700

    Introduction

---
+++ src/learning/Main.java
@@ -0,0 +1,14 @@
+package learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name ="Suhendri";
+        String age ="22 years old";
+        String address ="Takengon, Arul Gele. Aceh Tengah";
+
+        System.out.println("Name = " + name);
+        System.out.println("Age  = " + age );
+        System.out.println("My Address = " + address);
+    }
+}
