ad79ddaad1e0c9e27d2dfa23b05a971eb821bd91
ACER <hendry45xtc@gmail.com>
2021-03-11 20:18:23 +0700

    Variabel Assigment and Deklarasi

--- src/learning/Main.java
+++ src/learning/Main.java
@@ -10,5 +10,15 @@ public class Main {
         System.out.println("Name = " + name);
         System.out.println("Age  = " + age );
         System.out.println("My Address = " + address);
+
+     // variabel Asigment & Deklarasi
+            //Variabel Assigment
+            int a = 100;
+                System.out.printf("Nilai Assigment dari variabel a adalah %d \n",a);
+
+             //Variabel Deklarasi
+            int a1;
+            a1 = 50;
+                System.out.printf("Nilai Deklarasi dari variabel a1 adalah %d \n",a1);
     }
 }
