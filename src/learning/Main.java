4d885a5219e0a21e852dc83fc9dbb4ef93a85af8
ACER <hendry45xtc@gmail.com>
2021-03-13 15:08:36 +0700

    Menambahkan User Input

--- src/learning/Main.java
+++ src/learning/Main.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String name ="Suhendri";
-        String age ="22 years old";
-        String address ="Takengon, Arul Gele. Aceh Tengah";
+        //Membuat User Input..
 
-        System.out.println("Name = " + name);
-        System.out.println("Age  = " + age );
-        System.out.println("My Address = " + address);
+        System.out.println("==== Silahkan Input Username ====");
+        Scanner UserName = new Scanner(System.in);
+        String Nama;
+
+        System.out.print("Input Nama = ");
+        Nama = UserName.next();
+        System.out.println("Selamat datang " + Nama);
 
      // variabel Asigment & Deklarasi
             //Variabel Assigment
